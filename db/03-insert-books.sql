7163196b36f110f865588b21285fe6aafebadc02
Sojeong Park <macpark09@gmail.com>
2021-06-04 14:16:19 +0100

    add

--- db/03-insert-books.sql
+++ db/03-insert-books.sql
@@ -2,4 +2,10 @@
 INSERT INTO books
 ( title, price_in_pence, quantity_in_stock, release_date, is_fiction)
 VALUES
-('the little prince','699','1020','1943-04-06',true)
+('the little prince','699','1020','1943-04-06',true),
+('the tale of peter rabbit','599','1000','1902-10-01',true),
+('emma','522','390','1815-12-23',true),
+('the war of the worlds','250','17','1897-04-01',true),
+('captin corelli''s mandolin','999','0','1995-08-29',true),
+('a brief history of time','825','0','1988-04-01',false),
+('pride and prejudice','699','4','1813-01-28',true);
