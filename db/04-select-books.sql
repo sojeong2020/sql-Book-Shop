c009c96a906dfae8305aa8487274d019cbd58e72
Sojeong Park <macpark09@gmail.com>
2021-06-04 15:30:47 +0100

    update-4

--- db/04-select-books.sql
+++ db/04-select-books.sql
@@ -2,24 +2,30 @@
 
 \echo '\n Here are all the books that we have in stock:\n'
 
-
+SELECT * FROM books;
 
 \echo '\n Here are all the non-fiction books:\n'
 
-
+SELECT title FROM books WHERE is_fiction = false;
 
 \echo '\n Here are all the books released in the 1900s:\n'
 
-
+SELECT title, release_date FROM books WHERE release_date BETWEEN '1900-01-01' AND '2021-01-01';
 
 \echo '\n Here are all the books with "The" in the title:\n'
+
+SELECT title FROM books WHERE title LIKE 'the%'; 
 
 
 
 \echo '\n Here are the books sorted in alphabetical order:\n'
 
+SELECT * FROM books ORDER BY title ASC;
+
 
 
 \echo '\n Here is the most expensive book:\n'
+
+SELECT title, price_in_pence FROM books ORDER BY price_in_pence DESC limit 1;
 
 
