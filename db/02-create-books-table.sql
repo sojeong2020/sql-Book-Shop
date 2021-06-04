0d8fd8a6a5c8743e846c0183c10301826fe7d1b9
Sojeong Park <macpark09@gmail.com>
2021-06-04 13:25:03 +0100

    update1-3

--- db/02-create-books-table.sql
+++ db/02-create-books-table.sql
@@ -1,3 +1,12 @@
 \c my_bookshop;
 
 -- create the books table here
+
+CREATE TABLE books (
+    identifier SERIAL PRIMARY KEY,
+    title VARCHAR(40) NOT NULL,
+    price_in_pence INT,
+    quantity_in_stock INT,
+    release_date DATE,
+    is_fiction BOOL
+);
--- db/03-insert-books.sql
+++ db/03-insert-books.sql
@@ -1,1 +1,5 @@
 \c my_bookshop;
+INSERT INTO books
+( title, price_in_pence, quantity_in_stock, release_date, is_fiction)
+VALUES
+('the little prince','699','1020','1943-04-06',true)
