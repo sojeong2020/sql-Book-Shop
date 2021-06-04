826b7616853ec7d24c73e49df6fa9a128fcbae63
Sojeong Park <macpark09@gmail.com>
2021-06-04 15:50:59 +0100

    complete 05,06

--- db/05-delete-books.sql
+++ db/05-delete-books.sql
@@ -2,4 +2,5 @@
 
 \echo '\n These books have been removed from the data as they are out of stock:\n'
 
+DELETE FROM books WHERE quantity_in_stock =0;
 
--- db/06-update-books.sql
+++ db/06-update-books.sql
@@ -1,5 +1,5 @@
 \c my_bookshop
 
 \echo '\n These books have been put on sale, due to excess stock:\n'
-
+ UPDATE books SET price_in_pence = 399 WHERE identifier = 8;
 
